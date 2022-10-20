ba68cf0ddfddc50f1d58ec7b1dca358f636ac24d
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-10-21 01:21:29 +0530

    hello world

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,27 @@
-print("hello world")
+#Hello everyone My self Pranjal Singh. I'm Learning python. 
+
+#Day01- Hello world and data types
+
+print('Hello world')   #to print "Hello world"
+
+print(2 + 3)   # addition(+)  
+print(3 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+#I'm starting my journey from starting and I'll there with u till last
