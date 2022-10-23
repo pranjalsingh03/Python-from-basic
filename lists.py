69fe0e2f7f6d583def9297675910185e3b652ed9
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-10-23 10:32:59 +0530

    lists in pyhton

--- lists.py
+++ lists.py
@@ -3,11 +3,11 @@
 empty_list = list() # this is an empty list, no item in the list
 print(len(empty_list)) # 0
 
-fruits = ['banana', 'orange', 'mango', 'lemon']                     # list of fruits
-vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']      # list of vegetables
-animal_products = ['milk', 'meat', 'butter', 'yoghurt']             # list of animal products
-web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB'] # list of web technologies
-countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+fruits = ['banana', 'orange', 'mango', 'lemon']                            # list of fruits
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']             # list of vegetables
+animal_products = ['milk', 'meat', 'butter', 'yoghurt']                    # list of animal products
+web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB']       # list of web technologies
+countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']          # lists of countries
 
 # Print the lists and it length
 print('Fruits:', fruits)
@@ -23,12 +23,13 @@ print('Number of countries:', len(countries))
 # Modifying list
 
 fruits = ['banana', 'orange', 'mango', 'lemon'] 
-first_fruit = fruits[0] # we are accessing the first item using its index
-print(first_fruit)      # banana
+first_fruit = fruits[0]                         # we are accessing the first item using its index
+print(first_fruit)                              # banana
 second_fruit = fruits[1]
-print(second_fruit)     # orange
+print(second_fruit)                             # orange
 last_fruit = fruits[3]
-print(last_fruit) # lemon
+print(last_fruit)                               # lemon
+
 # Last index
 last_index = len(fruits) - 1
 last_fruit = fruits[last_index]
@@ -45,8 +46,8 @@ fruits = ['banana', 'orange', 'mango', 'lemon']
 all_fruits = fruits[0:4] # it returns all the fruits
 
 # this is also give the same result as the above
-all_fruits = fruits[0:] # if we don't set where to stop it takes all the rest
-orange_and_mango = fruits[1:3] # it does not include the end index
+all_fruits = fruits[0:]                         # if we don't set where to stop it takes all the rest
+orange_and_mango = fruits[1:3]                  # it does not include the end index
 orange_mango_lemon = fruits[1:]
 
 fruits = ['banana', 'orange', 'mango', 'lemon'] 
@@ -59,12 +60,12 @@ orange_mango_lemon = fruits[-3:]
 
 fruits = ['banana', 'orange', 'mango', 'lemon'] 
 fruits[0] = 'Avocado' 
-print(fruits)       #  ['avocado', 'orange', 'mango', 'lemon']
+print(fruits)                                  #  ['avocado', 'orange', 'mango', 'lemon']
 fruits[1] = 'apple'
-print(fruits)       #  ['avocado', 'apple', 'mango', 'lemon']
+print(fruits)                                  #  ['avocado', 'apple', 'mango', 'lemon']
 last_index = len(fruits) - 1
 fruits[last_index] = 'lime'
-print(fruits)        #  ['avocado', 'apple', 'mango', 'lime']
+print(fruits)                                  #  ['avocado', 'apple', 'mango', 'lime']
 
 # checking items
 fruits = ['banana', 'orange', 'mango', 'lemon']
