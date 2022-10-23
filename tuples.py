147a78178456c304d6f0d53bd302814a943b2005
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-10-23 19:06:16 +0530

    tuples in python

--- tuples.py
+++ tuples.py
@@ -1,6 +1,9 @@
 ## Day06 - Tuples
 
-'''A tuple is a collection of different data types which is ordered and unchangeable (immutable). Tuples are written with round brackets, (). Once a tuple is created, we cannot change its values. We cannot use add, insert, remove methods in a tuple because it is not modifiable (mutable). Unlike list, tuple has few methods. Methods related to tuples:
+'''A tuple is a collection of different data types which is ordered and unchangeable (immutable). 
+Tuples are written with round brackets, (). Once a tuple is created, we cannot change its values. 
+We cannot use add, insert, remove methods in a tuple because it is not modifiable (mutable). 
+Unlike list, tuple has few methods. Methods related to tuples:
 
 - tuple(): to create an empty tuple
 - count(): to count the number of a specified item in a tuple
@@ -12,3 +15,10 @@
 empty_tuple = ()
 # or using the tuple constructor
 empty_tuple = tuple()
+
+#Tuple with initial values
+  
+# syntax
+tpl = ('item1', 'item2','item3')
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
