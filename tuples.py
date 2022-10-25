34ce94501019809b30c2662afcedca3b129ae13d
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-10-25 18:39:04 +0530

    tuples in python

--- tuples.py
+++ tuples.py
@@ -76,3 +76,37 @@ fruits[0] = 'apple'
 print(fruits)     # ['apple', 'orange', 'mango', 'lemon']
 fruits = tuple(fruits)
 print(fruits)     # ('apple', 'orange', 'mango', 'lemon')
+
+### Slicing tuples
+
+# We can slice out a sub-tuple by specifying a range of indexes where to start and where to end in the tuple, the return value will be a new tuple with the specified items.
+
+ # Range of Positive Indexes
+
+  
+# Syntax
+tpl = ('item1', 'item2', 'item3','item4')
+all_items = tpl[0:4]         # all items
+all_items = tpl[0:]         # all items
+middle_two_items = tpl[1:3]  # does not include item at index 3
+
+  
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[0:4]    # all items
+all_fruits= fruits[0:]      # all items
+orange_mango = fruits[1:3]  # doesn't include item at index 3
+orange_to_the_rest = fruits[1:]
+
+ # Range of Negative Indexes
+
+  
+# Syntax
+tpl = ('item1', 'item2', 'item3','item4')
+all_items = tpl[-4:]         # all items
+middle_two_items = tpl[-3:-1]  # does not include item at index 3 (-1)
+
+  
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[-4:]    # all items
+orange_mango = fruits[-3:-1]  # doesn't include item at index 3
+orange_to_the_rest = fruits[-3:]
