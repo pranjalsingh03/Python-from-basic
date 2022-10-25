deeed9d25b7b91dc7531dbaf12c2aa1abfbcea6c
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-10-25 18:20:23 +0530

    tuples in python

--- tuples.py
+++ tuples.py
@@ -30,3 +30,34 @@ fruits = ('banana', 'orange', 'mango', 'lemon')
 # syntax
 tpl = ('item1', 'item2', 'item3')
 len(tpl)
+
+### Accessing Tuple Items
+
+# Positive Indexing
+# Similar to the list data type we use positive or negative indexing to access tuple items.
+
+
+# Syntax
+tpl = ('item1', 'item2', 'item3')
+first_item = tpl[0]
+second_item = tpl[1]
+
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_index =len(fruits) - 1
+last_fruit = fruits[last_index]
+
+# Negative indexing
+# Negative indexing means beginning from the end, -1 refers to the last item, -2 refers to the second last and the negative of the list/tuple length refers to the first item.
+
+# Syntax
+tpl = ('item1', 'item2', 'item3','item4')
+first_item = tpl[-4]
+second_item = tpl[-3]
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[-4]
+second_fruit = fruits[-3]
+last_fruit = fruits[-1]
