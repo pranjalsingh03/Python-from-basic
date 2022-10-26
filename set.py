63529d1481fabf53770e0911c7cb99e09ad649e5
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-10-26 23:52:43 +0530

    sets in python

--- set.py
+++ set.py
@@ -4,4 +4,23 @@
 
 ## Sets
 
-#Set is a collection of items. Let me take you back to your elementary or high school Mathematics lesson. The Mathematics definition of a set can be applied also in Python. Set is a collection of unordered and un-indexed distinct elements. In Python set is used to store unique items, and it is possible to find the _union_, _intersection_, _difference_, _symmetric difference_, _subset_, _super set_ and _disjoint set_ among sets.
+'''Set is a collection of items. Let me take you back to your elementary or high school Mathematics lesson. The Mathematics definition of a set can be applied also in Python. Set is a collection of unordered and un-indexed distinct elements. In Python set is used to store unique items, and it is possible to find the _union_, _intersection_, _difference_, _symmetric difference_, _subset_, _super set_ and _disjoint set_ among sets.'''
+
+### Creating a Set
+
+#We use curly brackets, {} to create a set or the *set()* built-in function.
+
+#Creating an empty set
+
+# syntax
+st = {}
+
+# or
+
+st = set()
+
+
+#Creating a set with initial items
+
+# syntax
+st = {'item1', 'item2', 'item3', 'item4'}
