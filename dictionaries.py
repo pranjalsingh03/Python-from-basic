6d0840db5abbf8543d6dbcdc07bd00dc622470b3
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-10-30 17:53:32 +0530

    dist

---
+++ dictionaries.py
@@ -0,0 +1,14 @@
+# 📘 Day 8
+
+## Dictionaries
+
+#A dictionary is a collection of unordered, modifiable(mutable) paired (key: value) data type.
+
+### Creating a Dictionary
+
+#To create a dictionary we use curly brackets, {} or the *dict()* built-in function.
+
+# syntax
+empty_dict = {}
+# Dictionary with data values
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
