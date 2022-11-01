b3d2075d4645511cd20854f2b2a6bb442df70613
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-11-01 22:00:37 +0530

    dict

--- dictionaries.py
+++ dictionaries.py
@@ -85,3 +85,109 @@ print(person['skills'])     # ['JavaScript', 'React', 'CSS', 'HTML', 'Python']
 print(person['skills'][0])  # JavaScript
 print(person['address']['street']) # Gorakhpur 
 print(person['city'])       # Error
+
+#Accessing an item by key name raises an error if the key does not exist. To avoid this error first we have to check if a key exist or we can use the _get_ method. The get method returns None, which is a NoneType object data type, if the key does not exist.
+
+person = {
+    'first_name':'Pranjal',
+    'last_name':'Singh',
+    'age':18,
+    'country':'India',
+    'is_marred':False,
+    'skills':['JavaScript', 'React', 'CSS', 'HTML', 'Python'],
+    'address':{
+        'street':'Gorakhpur',
+        'zipcode':'274207'
+    }
+    }
+print(person.get('first_name')) # Asabeneh
+print(person.get('country'))    # Finland
+print(person.get('skills')) #['HTML','CSS','JavaScript', 'React', 'Node', 'MongoDB', 'Python']
+print(person.get('city'))   # None
+
+### Adding Items to a Dictionary
+
+#We can add new key and value pairs to a dictionary
+
+
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+dct['key5'] = 'value5'
+
+
+#Example:
+person = {
+    'first_name':'Pranjal',
+    'last_name':'Singh',
+    'age':18,
+    'country':'India',
+    'is_marred':False,
+    'skills':['JavaScript', 'React', 'CSS', 'HTML', 'Python'],
+    'address':{
+        'street':'Gorakhpur',
+        'zipcode':'274207'
+    }
+    }
+
+### Modifying Items in a Dictionary
+
+#We can modify items in a dictionary
+
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+dct['key1'] = 'value-one'
+
+#Example:
+
+person = {
+    'first_name':'Pranjal',
+    'last_name':'Singh',
+    'age':18,
+    'country':'India',
+    'is_marred':False,
+    'skills':['JavaScript', 'React', 'CSS', 'HTML', 'Python'],
+    'address':{
+        'street':'Gorakhpur',
+        'zipcode':'274207'
+    }
+    }
+person['first_name'] = 'Eyob'
+person['age'] = 252
+
+### Checking Keys in a Dictionary
+
+#We use the _in_ operator to check if a key exist in a dictionary
+
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+print('key2' in dct) # True
+print('key5' in dct) # False
+
+### Removing Key and Value Pairs from a Dictionary
+
+#_pop(key)_: removes the item with the specified key name:
+#_popitem()_: removes the last item
+#_del_: removes an item with specified key name
+
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+dct.pop('key1') # removes key1 item
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+dct.popitem() # removes the last item
+del dct['key2'] # removes key2 item
+
+
+#Example:
+
+person = {
+    'first_name':'Pranjal',
+    'last_name':'Singh',
+    'age':18,
+    'country':'India',
+    'is_marred':False,
+    'skills':['JavaScript', 'React', 'CSS', 'HTML', 'Python'],
+    'address':{
+        'street':'Gorakhpur',
+        'zipcode':'274207'
+    }
+    }
