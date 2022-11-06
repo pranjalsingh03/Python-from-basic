a67cb4597d9ebfbef0335a37b9bc8ef8fa41664a
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-11-06 22:39:00 +0530

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,12 @@
+# Day09-Conditionals
+
+'''Conditionals'''
+
+#By default, statements in Python script are executed sequentially from top to bottom. If the processing logic require so, the sequential flow of execution can be altered in two way:
+
+# Conditional execution: a block of one or more statements will be executed if a certain expression is true
+# Repetitive execution: a block of one or more statements will be repetitively executed as long as a certain expression is true. In this section, we will cover _if_, _else_, _elif_ statements. The comparison and logical operators we learned in previous sections will be useful here.
+
+'''If Condition'''
+
+#In python and other programming languages the key word _if_ is used to check if a condition is true and to execute the block code. Remember the indentation after the colon.
