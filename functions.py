e0f848c147043697bdf00adbf90677041567a908
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-11-16 23:41:13 +0530

    functions in python

---
+++ functions.py
@@ -0,0 +1,3 @@
+## Functions
+
+'''So far we have seen many built-in Python functions. In this section, we will focus on custom functions. What is a function? Before we start making functions, let us learn what a function is and why we need them?'''
