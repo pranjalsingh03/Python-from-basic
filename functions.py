93aa6cba5447135a50eba8c8034177f7a1393bf8
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-11-16 23:43:43 +0530

    funct

--- functions.py
+++ functions.py
@@ -1,3 +1,11 @@
 ## Functions
 
 '''So far we have seen many built-in Python functions. In this section, we will focus on custom functions. What is a function? Before we start making functions, let us learn what a function is and why we need them?'''
+
+### Defining a Function
+
+'''A function is a reusable block of code or programming statements 
+designed to perform a certain task. To define or declare a function, 
+Python provides the _def_ keyword. The following is the syntax for 
+defining a function. The function block of code is executed only if 
+the function is called or invoked.'''
