112d7769e0ae200b60a68d95fb0bdf47d759b4b4
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-11-16 23:50:17 +0530

    function in python

--- functions.py
+++ functions.py
@@ -22,3 +22,24 @@ def function_name():
 
 # Calling a function
     function_name()
+
+### Function without Parameters
+
+'''Function can be declared without parameters.'''
+
+#Example:
+
+def generate_full_name ():
+    first_name = 'Pranjal'
+    last_name = 'Singh'
+    space = ' '
+    full_name = first_name + space + last_name
+    print(full_name)
+generate_full_name ()                 # calling a function
+
+def add_two_numbers ():
+    num_one = 2
+    num_two = 3
+    total = num_one + num_two
+    print(total)
+add_two_numbers()
