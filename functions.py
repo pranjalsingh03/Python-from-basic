06ebe50943a6e578671c2367e33ff9a661ecccba
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-11-17 00:00:54 +0530

    function in python

--- functions.py
+++ functions.py
@@ -67,3 +67,52 @@ def add_two_numbers ():
     total = num_one + num_two
     return total
 print(add_two_numbers())
+
+### Function with Parameters
+
+'''In a function we can pass different data types(number, string, 
+boolean, list, tuple, dictionary or set) as a parameter'''
+
+#- Single Parameter: If our function takes a parameter we should call our function with an argument
+
+
+# syntax
+# Declaring a function
+def function_name(parameter):
+#    codes
+#   codes
+# Calling function
+    print(function_name(argument))
+
+
+#Example:
+
+def greetings (name):
+    message = name + ', welcome to Python for Everyone!'
+    return message
+
+print(greetings('Asabeneh'))
+
+def add_ten(num):
+    ten = 10
+    return num + ten
+print(add_ten(90))
+
+def square_number(x):
+    return x * x
+print(square_number(2))
+
+def area_of_circle (r):
+    PI = 3.14
+    area = PI * r ** 2
+    return area
+print(area_of_circle(10))
+
+def sum_of_numbers(n):
+    total = 0
+    for i in range(n+1):
+        total+=i
+    print(total)
+print(sum_of_numbers(10)) # 55
+print(sum_of_numbers(100)) # 5050
+
