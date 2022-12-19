b851da1c1ab2e117728137c2e6ea31f4b11bfe30
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-12-19 23:29:11 +0530

    modules

--- modules.py
+++ modules.py
@@ -1,2 +1,10 @@
 # 📘 Day 12 
 # Modules
+
+# What is a Module
+
+'''A module is a file containing a set of codes or a set of functions which can be included to an application. A module could be a file containing a single variable, a function or a big code base.'''
+
+# Creating a Module
+
+'''To create a module we write our codes in a python script and we save it as a .py file. Create a file named mymodule.py inside your project folder. Let us write some code in this file.'''
