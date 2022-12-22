335cbf27edfc02987eb6ec0c738a996ce231ceb0
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-12-23 00:30:59 +0530

    functions

--- modules.py
+++ modules.py
@@ -8,3 +8,6 @@
 # Creating a Module
 
 '''To create a module we write our codes in a python script and we save it as a .py file. Create a file named mymodule.py inside your project folder. Let us write some code in this file.'''
+
+def generate_full_name(firstname, lastname):
+    return firstname + ' ' + lastname
