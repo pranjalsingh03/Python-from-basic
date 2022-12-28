83129123ec4d24155d0aba717b85aff295432dde
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-12-28 20:42:23 +0530

    list compreherension

--- list_comprehension.py
+++ list_comprehension.py
@@ -12,3 +12,8 @@ language = 'Python'
 lst = list(language) # changing the string to list
 print(type(lst))     # list
 print(lst)           # ['P', 'y', 't', 'h', 'o', 'n']
+
+# Second way: list comprehension
+lst = [i for i in language]
+print(type(lst)) # list
+print(lst)       # ['P', 'y', 't', 'h', 'o', 'n']
