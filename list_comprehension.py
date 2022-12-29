2b6a3edd8ee4a644d7aa9e683422ab8a088f5adc
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-12-29 20:36:21 +0530

    list comperhension

--- list_comprehension.py
+++ list_comprehension.py
@@ -33,3 +33,16 @@ print(squares)                    # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 # It is also possible to make a list of tuples
 numbers = [(i, i * i) for i in range(11)]
 print(numbers)                             # [(0, 0), (1, 1), (2, 4), (3, 9), (4, 16), (5, 25)]
+
+#Example:2
+
+'''List comprehension can be combined with if expression'''
+
+
+# Generating even numbers
+even_numbers = [i for i in range(21) if i % 2 == 0]  # to generate even numbers list in range 0 to 21
+print(even_numbers)                    # [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+# Generating odd numbers
+odd_numbers = [i for i in range(21) if i % 2 != 0]  # to generate odd numbers in range 0 to 21
+print(odd_numbers)                      # [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
